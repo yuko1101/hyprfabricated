0b9035704753481a5c923158fc05257c728a8412
Axenide <axenide@gmail.com>
2025-02-25 20:43:50 -0300

    Refactor volume control by creating VolumeSmall widget and updating styles

--- modules/bar.py
+++ modules/bar.py
@@ -6,90 +6,12 @@ from fabric.widgets.button import Button
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.hyprland.widgets import Workspaces, WorkspaceButton
 from fabric.utils.helpers import get_relative_path, exec_shell_command_async
-from gi.repository import GLib, Gdk
+from gi.repository import Gdk
 from modules.systemtray import SystemTray
-from config.config import open_config
 import modules.icons as icons
 import modules.data as data
 from modules.battery import Battery
-
-from fabric.widgets.overlay import Overlay
-from fabric.widgets.eventbox import EventBox
-from fabric.widgets.circularprogressbar import CircularProgressBar
-from fabric.audio.service import Audio
-
-class VolumeWidget(Box):
-    def __init__(self, **kwargs):
-        super().__init__(name="button-bar-vol", **kwargs)
-        self.audio = Audio()
-
-        self.progress_bar = CircularProgressBar(
-            name="button-volume", pie=False, size=28, line_width=2,
-        )
-        self.ico = icons.vol_high
-        self.vollabel = Label(name="button-bar-label", markup=icons.vol_high)
-
-        self.volbutton = Button(
-                on_clicked=self.toggle_mute,
-                child=self.vollabel
-        )
-        self.event_box = EventBox(
-#            name="button-bar-vol",
-            events="scroll",
-            child=Overlay(
-                child=self.progress_bar,
-                overlays=self.volbutton
-            ),
-        )
-
-        self.audio.connect("notify::speaker", self.on_speaker_changed)
-        self.event_box.connect("scroll-event", self.on_scroll)
-        self.add(self.event_box)
-
-    #        self.button.connect("button", self.on_clicked)
-
-    #thanks https://github.com/rubiin/HyDePanel/blob/master/widgets/volume.python.py
-
-    def toggle_mute(self, event):
-        current_stream = self.audio.speaker
-        if current_stream:
-            current_stream.muted = not current_stream.muted
-            self.volbutton.get_child().set_markup(icons.vol_off) if current_stream.muted else self.on_speaker_changed()
-    
-    #def Mute():
-    #self.audio.speaker += 2,
-
-    def on_scroll(self, _, event):
-        match event.direction:
-            case 0:
-                self.audio.speaker.volume += 2
-            case 1:
-                self.audio.speaker.volume -= 2
-        if self.audio.speaker.volume > 60:
-            self.volbutton.get_child().set_markup(icons.vol_high)
-        elif self.audio.speaker.volume > 30:
-            self.volbutton.get_child().set_markup(icons.vol_medium)
-        elif self.audio.speaker.volume < 30:
-            self.volbutton.get_child().set_markup(icons.vol_mute)
-
-
-        return
-
-    def on_speaker_changed(self, *_):
-        if not self.audio.speaker:
-            return
-        self.progress_bar.value = self.audio.speaker.volume / 100
-        self.audio.speaker.bind(
-            "volume", "value", self.progress_bar, lambda _, v: v / 100
-        )
-        if self.audio.speaker.volume > 60:
-            self.volbutton.get_child().set_markup(icons.vol_high)
-        elif self.audio.speaker.volume > 30:
-            self.volbutton.get_child().set_markup(icons.vol_medium)
-        elif self.audio.speaker.volume < 30:
-            self.volbutton.get_child().set_markup(icons.vol_mute)
-        return
-
+from modules.volume import VolumeSmall
 
 class Bar(Window):
     def __init__(self, **kwargs):
@@ -178,7 +100,7 @@ class Bar(Window):
         )
 
 
-        self.volume = VolumeWidget()
+        self.volume = VolumeSmall()
         self.bar_inner = CenterBox(
             name="bar-inner",
             orientation="h",
@@ -200,7 +122,7 @@ class Bar(Window):
                 orientation="h",
                 children=[
                     self.volume,
-#                    self.battery,
+                    self.battery,
                     self.button_color,
                     self.systray,
                     self.button_config,
