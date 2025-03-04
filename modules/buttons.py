851addedbcf11ef4ee9c48115330de0413100baf
Axenide <axenide@gmail.com>
2025-03-04 18:48:19 -0300

    Removed Vte

--- modules/buttons.py
+++ modules/buttons.py
@@ -7,7 +7,6 @@ from fabric.utils.helpers import exec_shell_command_async
 import gi
 from gi.repository import Gtk, Gdk, GLib  # Added GLib import
 gi.require_version('Gtk', '3.0')
-gi.require_version('Vte', '2.91')
 import modules.icons as icons
 from services.network import NetworkClient
 
