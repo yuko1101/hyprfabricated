9ea8b98f57fe373d5a8ec3f2ff1e367d8c5758de
Axenide <axenide@gmail.com>
2025-04-07 19:26:50 -0300

    Removed seconds

--- modules/bar.py
+++ modules/bar.py
@@ -68,7 +68,7 @@ class Bar(Window):
         self.on_language_switch()
         self.connection.connect("event::activelayout", self.on_language_switch)
 
-        self.date_time = DateTime(name="date-time", formatters=["%H:%M:%S"] if not data.VERTICAL else ["%H\n%M"], h_align="center" if not data.VERTICAL else "fill", v_align="center", h_expand=True, v_expand=True)
+        self.date_time = DateTime(name="date-time", formatters=["%H:%M"] if not data.VERTICAL else ["%H\n%M"], h_align="center" if not data.VERTICAL else "fill", v_align="center", h_expand=True, v_expand=True)
 
         self.button_apps = Button(
             name="button-bar",
